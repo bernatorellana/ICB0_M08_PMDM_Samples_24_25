11a7e2adb46462415d39fde3a0b9806d86b58736
developrepogithub <develop.repo.github@gmail.com>
2024-09-20 18:05:28 +0200

    Afegim botonera activa i les imatges

--- UF1/20240917_LayoutsApp/app/src/main/java/org/milaifontanals/layoutsapp/MainActivity.java
+++ UF1/20240917_LayoutsApp/app/src/main/java/org/milaifontanals/layoutsapp/MainActivity.java
@@ -1,14 +1,126 @@
 package org.milaifontanals.layoutsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import org.milaifontanals.layoutsapp.model.Persona;
 
 public class MainActivity extends AppCompatActivity {
+
+// Views del layout
+    private ImageView imgPhoto;
+
+    private EditText edtNom;
+    private EditText edtCognoms;
+    private EditText edtNIF;
+    private RadioGroup rdgSexe;
+
+    private RadioButton rdoH;
+    private RadioButton rdoD;
+    private RadioButton rdoA;
+    private Spinner spnProvincies;
+    private LinearLayout llyNext;
+    private LinearLayout llyPrev;
+
+
+
+    private int indexPersonatgeActual = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        creacioIds();
+        //--------------------------------------------------------------
+        showCurrentUser();
+
+        /*llyNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // codi del click
+            }
+        });*/
+        llyNext.setOnClickListener(view -> {
+            //codi del click
+            this.indexPersonatgeActual++;
+            this.indexPersonatgeActual =
+                    this.indexPersonatgeActual%Persona.getPersones().size();
+            showCurrentUser();
+        });
+        llyPrev.setOnClickListener(view -> {
+            //codi del click
+            this.indexPersonatgeActual--;
+            if(this.indexPersonatgeActual<0) {
+                this.indexPersonatgeActual = Persona.getPersones().size() - 1;
+            }
+            showCurrentUser();
+        });
+    }
+
+    private void creacioIds() {
+        //-----------------------------------------------------------
+        // Enllaçar els elements de la interfíce gràfica
+        imgPhoto = findViewById(R.id.imgPhoto);
+        edtNom = findViewById(R.id.edtNom);
+        edtCognoms = findViewById(R.id.edtCognoms);
+        edtNIF = findViewById(R.id.edtNIF);
+        rdgSexe = findViewById(R.id.rdgSexe);
+        rdoH = findViewById(R.id.rdoH);
+        rdoD = findViewById(R.id.rdoD);
+        rdoA = findViewById(R.id.rdoA);
+        spnProvincies = findViewById(R.id.spnProvincies);
+        llyNext = findViewById(R.id.llyNext);
+        llyPrev = findViewById(R.id.llyPrev);
+    }
+
+    private void showCurrentUser() {
+       Persona actual = Persona.getPersones().get(indexPersonatgeActual);
+
+       imgPhoto.setImageResource(actual.getImatge());
+       edtNom.setText(actual.getNom());
+       edtCognoms.setText(actual.getCognoms());
+       edtNIF.setText(actual.getNIF());
+        /*switch (actual.getSexe()){
+            case DONA: rdoD.setChecked(true);break;
+            case HOME: rdoH.setChecked(true);break;
+            case ALTRES: rdoA.setChecked(true);break;
+        }*/
+        //rdgSexe.check();
+       RadioButton rb = (RadioButton)rdgSexe.getChildAt(actual.getSexe().ordinal());
+       rb.setChecked(true);
+
+        boolean prevActivat = (indexPersonatgeActual>0);
+        boolean nextActivat = indexPersonatgeActual<Persona.getPersones().size()-1;
+        activa(llyPrev, prevActivat);
+        activa(llyNext, nextActivat);
+    }
+    private void activa(Button b, boolean prevActivat) {
+        b.setEnabled(prevActivat);
+    }
+    private void activa(LinearLayout llyPrev, boolean prevActivat) {
+
+        //android:backgroundTint="#88888800"
+        //android:backgroundTintMode="multiply"
+        llyPrev.setBackgroundTintMode(PorterDuff.Mode.MULTIPLY);
+        if(!prevActivat) {
+            llyPrev.setBackgroundTintList(ColorStateList.valueOf(Color.argb(100,200,200,200)));
+        } else {
+            llyPrev.setBackgroundTintList(null);
+        }
     }
 }
--- UF1/20240917_LayoutsApp/app/src/main/java/org/milaifontanals/layoutsapp/model/Persona.java
+++ UF1/20240917_LayoutsApp/app/src/main/java/org/milaifontanals/layoutsapp/model/Persona.java
@@ -1,5 +1,9 @@
 package org.milaifontanals.layoutsapp.model;
 
+import androidx.annotation.DrawableRes;
+
+import org.milaifontanals.layoutsapp.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,22 +14,32 @@ public class Persona {
     public static List<Persona> getPersones(){
         if(persones == null) {
             persones = new ArrayList<>();
-            persones.add(new Persona("PacoX", "Gutiérrez", "1111111H",Sexe.ALTRES, null));
-            persones.add(new Persona("Maria", "Nana", "2222222H",Sexe.DONA, null));
-            persones.add(new Persona("Joan", "Gómez", "3333333H",Sexe.HOME, null));
-            persones.add(new Persona("Pep", "Pérez", "144444444H",Sexe.HOME, null));
-            persones.add(new Persona("Sara", "Sánchez", "5555555H",Sexe.ALTRES, null));
+            persones.add(new Persona(R.drawable.gremlin, "PacoX","Gutiérrez", "1111111H",Sexe.DONA, null));
+            persones.add(new Persona(R.drawable.female1,"Maria", "Nana", "2222222H",Sexe.DONA, null));
+            persones.add(new Persona(R.drawable.gizmo,"Joan", "Gómez", "3333333H",Sexe.HOME, null));
+            persones.add(new Persona(R.drawable.orejotas,"Pep", "Pérez", "144444444H",Sexe.HOME, null));
+            persones.add(new Persona(R.drawable.female2,"Sara", "Sánchez", "5555555H",Sexe.ALTRES, null));
         }
         return persones;
     }
 
+    public int getImatge() {
+        return imatge;
+    }
+
+    public void setImatge(@DrawableRes int imatge) {
+        this.imatge = imatge;
+    }
+
+    private @DrawableRes int imatge;
     private String nom;
     private String cognoms;
     private String NIF;
     private Sexe sexe;
     private Provincia provincia;
 
-    public Persona(String nom, String cognoms, String NIF, Sexe sexe, Provincia provincia) {
+    public Persona(@DrawableRes int imatge,  String nom, String cognoms, String NIF, Sexe sexe, Provincia provincia) {
+        this.imatge = imatge;
         this.nom = nom;
         this.cognoms = cognoms;
         this.NIF = NIF;
